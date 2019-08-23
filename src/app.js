7001d0fc7fd379acce12ea5abfac18d1dae292a8
vananda <venothanand91@gmail.com>
2019-08-24 01:02:43 +0530

    setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const request       = require('request')
 const path          = require('path');
 const express       = require('express');
 const hbs           = require('hbs')
-const port          = 3000 ;
+const port          = process.env.PORT || 3000 ;
 const forecast      = require('./Utils/forecast');
 const geoCode       = require('./Utils/geoCode')
 
